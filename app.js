1704e4bbf391ebc4c7356d734908c21e86c5f76b
zhouzhoustar <690571152@qq.com>
2019-04-29 19:13:19 +0800

    实现了双保险验证，如果没有权限就强制跳转到login

--- app.js
+++ app.js
@@ -10,4 +10,10 @@ app.get("/mingan",(req,res)=>{
 	res.json({"err" : -4});
 });
 
+app.get("/me",(req,res)=>{
+	// res.status(401)
+	// res.json({"err" : -4});
+	res.json({'username':'xiaoming','nickname':'小明','avatar':'dfaf.jpg'})
+});
+
 app.listen(3000);
